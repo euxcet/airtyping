a548ff0e3cf285b5366325fe860af44980071693
thuliu-yt16 <liu-yt16@mails.tsinghua.edu.cn>
2018-11-04 23:00:00 +0800

    support windows python 2.7 32bit

--- leap.py
+++ leap.py
@@ -2,3 +2,6 @@ import sys
 if (sys.platform == "darwin"):
 	import lib.Leap as Leap
 	from lib.Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+elif (sys.platform == "win32"):
+	import windows.Leap as Leap
+	from windows.Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
